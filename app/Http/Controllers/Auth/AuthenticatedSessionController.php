9abe2334007fe498e674b0774d88d03d873bde04
mrrprajith <prajith@benagon.com>
2025-06-17 22:25:19 +0530

    Update AuthenticatedSessionController.php

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,7 +29,7 @@ class AuthenticatedSessionController extends Controller
         $request->session()->regenerate();
 
         //return redirect()->intended(route('dashboard', absolute: false));
-        return $request->user()->is_admin ? redirect('/admin/dashboard') : redirect(RouteServiceProvider::HOME);
+        return $request->user()->is_admin ? redirect('/admin/dashboard') : redirect('/dashboard');;
     }
 
     /**
