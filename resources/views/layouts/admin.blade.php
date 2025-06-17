e6c5b128884d1b85c76d25134e03a2dbfd367a53
mrrprajith <prajith@benagon.com>
2025-06-17 23:11:33 +0530

    added title

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>Admin Panel</title>
+        <title>Admin Panel : {{ config('app.name', '') }}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
